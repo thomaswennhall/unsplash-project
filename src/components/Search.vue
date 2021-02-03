8ac69220a8cadbc9b9c1908d3bc908e1495c3b7a
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-02-03 10:12:28 +0100

    Add styling to search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -29,10 +29,39 @@ export default {
 
 <style lang="scss" scoped>
 .search-bar {
-  input,
-  button {
-    font-family: inherit;
-    padding: 0.6rem;
+  
+  form{
+    width: 100%;
+
+    input,
+    button {
+      font-family: inherit;
+      outline: none;
+
+      padding: 0.6rem;
+      border: 0.15rem solid #2c3e50;
+    }
+
+    input{
+      width: 60%;
+      border-right: none;
+      border-radius: 0.3rem 0 0 0.3rem;
+    }
+    button{
+      font-weight: 500;
+      color: #2c3e50;
+      border-radius: 0 0.3rem 0.3rem 0;
+      background-color: #fff;
+
+      &:hover{
+        background-color: #2c3e50;
+        color: white;
+      }
+      &:active{
+        background-color: #fff;
+        color: #2c3e50;
+      }
+    }
   }
 }
 </style>
