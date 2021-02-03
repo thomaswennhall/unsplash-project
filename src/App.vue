c3a519f36056c6546d59fe8f3983b1607e0483bf
richard-iths <richard.forsberg@iths.se>
2021-02-03 10:53:37 +0100

    started on mockup js module, modified data.json

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>Oplask</h1>
-    <SearchBar v-on:search="search"/>
+    <SearchBar v-on:search="search" />
   </div>
 </template>
 
@@ -9,26 +9,19 @@
 import SearchBar from "@/components/Search.vue";
 
 export default {
-<<<<<<< HEAD
   name: "App",
-  components: { SearchBar },
-};
-=======
-  name: 'App',
   components: { SearchBar },
 
   methods: {
-    search(input){
+    search(input) {
       console.log(input);
-    }
-  }
-  
-}
->>>>>>> 8ac69220a8cadbc9b9c1908d3bc908e1495c3b7a
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300;400&display=swap");
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -38,11 +31,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 
-  h1{
-    font-family: 'Londrina Solid', Avenir, Helvetica, Arial, sans-serif;
+  h1 {
+    font-family: "Londrina Solid", Avenir, Helvetica, Arial, sans-serif;
     font-size: 3rem;
     font-weight: 400;
-    letter-spacing: 5px; 
+    letter-spacing: 5px;
   }
 }
 </style>
--- src/api/mockup.js
+++ src/api/mockup.js
@@ -1,5 +1,18 @@
 import data from "./data.json";
 
-export const getData = () => {
-  this.axios.get();
+const pagination = {
+  numPages: 0,
+  perPage: 0,
+  prevPage: 0,
+  nextPage: 0,
+};
+const BASE_URL = "https://api.unsplash.com/photos/";
+export const getData = (perPage) => {
+  const url = BASE_URL.concat(
+    `?client_id=${process.env.VUE_UNSPLASHED_KEY}&per_page=${perPage}`
+  );
+  console.log("The url will be", url);
+  pagination.numPages = Number(data["x-total"]) / Number(data["x-per-page"]);
+
+  return data.data;
 };
