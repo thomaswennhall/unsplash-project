44b5962e73c7caea1593f0181ac284e4afd94b8e
richard-iths <richard.forsberg@iths.se>
2021-02-10 09:27:08 +0100

    Added vuex to project via vue add vuex command

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import store from './store'
 
 Vue.config.productionTip = false;
 
@@ -9,5 +10,7 @@ new Vue({
       favorites: [],
     };
   },
-  render: (h) => h(App),
+
+  store,
+  render: (h) => h(App)
 }).$mount("#app");
